19e7d90ff6dca6ae79ae39c88aef94af8eedd13e
Alex Landolt <alex.landolt@gmail.com>
2020-08-05 19:43:55 +0200

    new version numbering system

--- include/version.h
+++ include/version.h
@@ -1,6 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define SOFTWARE_VERSION "1.2.0"
+#define BRANCH "master"
+#define VERSION_NUMBER "1.2.0"
+#define SPACER ", "
+#define SOFTWARE_VERSION VERSION_NUMBER SPACER BRANCH
 
 #endif
